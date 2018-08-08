3d78a40ea6e0ec6bcfeca2f5f0b581e641191414
Diego Zamboni <diego@zzamboni.org>
2018-08-09 00:26:01 +0200

    Removed docs

--- docs/src/ch03/sshd_config1.sh
+++
@@ -1,1 +0,0 @@
-echo "PermitRootLogin no" >> /etc/ssh/sshd_config
--- docs/src/ch03/sshd_config2.sh
+++
@@ -1,3 +0,0 @@
-(grep -iq 'PermitRootLogin' /etc/ssh/sshd_config ||
- echo "PermitRootLogin no" >> /etc/ssh/sshd_config) &&
- sed -i 's/^.*PermitRootLogin.*$/PermitRootLogin no/;' /etc/ssh/sshd_config
